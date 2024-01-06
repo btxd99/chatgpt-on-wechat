3f8ff8a51d14f312f81725c6fb23ebf006568769
btxd99 <gy150588888@sina.com>
2024-01-06 21:12:48 +0800

    Revert "Add plugin humantone"
    This reverts commit 42edb0689bf99a414059b8d3d09299088eb5c8ba.

--- channel/wechat/wechat_channel.py
+++ channel/wechat/wechat_channel.py
@@ -187,15 +187,8 @@ class WechatChannel(ChatChannel):
     def send(self, reply: Reply, context: Context):
         receiver = context["receiver"]
         if reply.type == ReplyType.TEXT:
-            # 遇到"\n \n \n"就拆分消息
-            reply_list = re.split('\n \n \n', reply.content)
-            for each_line in reply_list:
-                reply.content = each_line.strip()
-                # 模拟打字速度
-                time.sleep(1 + 0.7 * len(each_line))
-                logger.debug("Typing {} words, sleep {} s".format(len(each_line), 1 + 0.7 * len(each_line)))
-                itchat.send(reply.content, toUserName=receiver)
-                logger.info("[WX] sendMsg={}, receiver={}".format(reply, receiver))
+            itchat.send(reply.content, toUserName=receiver)
+            logger.info("[WX] sendMsg={}, receiver={}".format(reply, receiver))
         elif reply.type == ReplyType.ERROR or reply.type == ReplyType.INFO:
             itchat.send(reply.content, toUserName=receiver)
             logger.info("[WX] sendMsg={}, receiver={}".format(reply, receiver))
--- start.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start /B cmd /C call python D:\code\chatgpt-on-wechat\app.py &
